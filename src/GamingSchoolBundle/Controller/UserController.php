4446b519cf7e6d07a4bdce58021a26c395c1e35e
ThibaultCrosnier <thibault.crosnier@gmail.com>
2016-03-03 16:51:55 +0100

    Réparation de la page coach

--- src/GamingSchoolBundle/Controller/UserController.php
+++ src/GamingSchoolBundle/Controller/UserController.php
@@ -18,32 +18,32 @@ class UserController extends Controller
      */
     public function coachesByGameAction($game_slug)
     {
-     $userRepository = $this
-    ->getDoctrine()
-    ->getManager()
-    ->getRepository('GamingSchoolBundle:User')
-  ;
+    	$userRepository = $this
+    	->getDoctrine()
+    	->getManager()
+    	->getRepository('GamingSchoolBundle:User')
+    	;
 
-  $gameRepository = $this
-    ->getDoctrine()
-    ->getManager()
-    ->getRepository('GamingSchoolBundle:game')
-  ;
+    	$gameRepository = $this
+    	->getDoctrine()
+    	->getManager()
+    	->getRepository('GamingSchoolBundle:game')
+    	;
 
-  $game = $gameRepository->findOneBy(array('gameSlug' => $game_slug));
-  $listCoaches = $userRepository->findByCoaches($game->getId());
-  $data["game"] = $game->getGameName();
+    	$game = $gameRepository->findOneBy(array('gameSlug' => $game_slug));
+    	$listCoaches = $userRepository->findByCoaches($game->getId());
+    	$data["game"] = $game->getGameName();
 
-  foreach ($listCoaches as $coach) {
-   $data["listCoaches"][] = array(
-    'id' => $coach->getId(),
-     'username' => $coach->getUsername(),
-     'firstname' => $coach->getUserFirstName(),
-     'lastname' => $coach ->getUserLastName(),
-     'email' => $coach->getEmail()
-   );
-  }
-        return $this->render('GamingSchoolBundle:Default:listcoaches.html.twig', $data);
+    	foreach ($listCoaches as $coach) {
+    		$data["listCoaches"][] = array(
+    			'id' => $coach->getId(),
+    			'username' => $coach->getUsername(),
+    			'firstname' => $coach->getUserFirstName(),
+    			'lastname' => $coach ->getUserLastName(),
+    			'email' => $coach->getEmail()
+    			);
+    	}
+    	return $this->render('GamingSchoolBundle:Default:listcoaches.html.twig', $data);
     }
 
     /**
@@ -51,45 +51,45 @@ class UserController extends Controller
      */
     public function userProfileAction(Request $request, $user_id)
     {
-     $userRepository = $this
-    ->getDoctrine()
-    ->getManager()
-    ->getRepository('GamingSchoolBundle:User')
-  ;
+    	$userRepository = $this
+    	->getDoctrine()
+    	->getManager()
+    	->getRepository('GamingSchoolBundle:User')
+    	;
 
-  $infosUser = $userRepository->find($user_id);
-  $data["infos"] = $infosUser;
-  
-  if($infosUser->hasRole('ROLE_COACH'))
-   return $this->redirect('/profile/coach/'.$user_id.'');
-  else if($infosUser->hasRole('ROLE_USER'))
-   return $this->render('GamingSchoolBundle:Default:profile.html.twig', $data);
- }
+    	$infosUser = $userRepository->find($user_id);
+    	$data["infos"] = $infosUser;
+
+    	if($infosUser->hasRole('ROLE_COACH'))
+    		return $this->redirect('/profile/coach/'.$user_id.'');
+    	else if($infosUser->hasRole('ROLE_USER'))
+    		return $this->render('GamingSchoolBundle:Default:profile.html.twig', $data);
+    }
     
     /**
      * @Route("/profile/coach/{coach_id}")
      */
     public function coachByIdAction($coach_id, Request $request)
     {
-        $userRepository = $this
-          ->getDoctrine()
-          ->getManager()
-          ->getRepository('GamingSchoolBundle:User')
-        ;
-        $data['coach'] = $userRepository->find($coach_id);
-        
-        $packs = $data['coach']->getCoachingPack();
-        foreach ($packs as $pack) {
-            $data["packs"][] = array(
-                'id' => $pack->getId(),
-                'nb_hours' => $pack->getCoachingPackNbHours(),
-                'price' => $pack->getCoachingPackPrice(),
-            );
-        }
-        $form = $this->createForm(SelectPack::Class, $data["packs"])->createView();
-        
+    	$userRepository = $this
+    	->getDoctrine()
+    	->getManager()
+    	->getRepository('GamingSchoolBundle:User')
+    	;
+    	$data['coach'] = $userRepository->find($coach_id);
+
+    	$packs = $data['coach']->getCoachingPack();
+    	foreach ($packs as $pack) {
+    		$data["packs"][] = array(
+    			'id' => $pack->getId(),
+    			'nb_hours' => $pack->getCoachingPackNbHours(),
+    			'price' => $pack->getCoachingPackPrice(),
+    			);
+    	}
+    	$form = $this->createForm(SelectPack::Class, $data["packs"])->createView();
+
         //$form->handleRequest($request);
-        
+
        /* if($form->isSubmitted() && $form->isValid()){
             $em = $this->get('doctrine')->getManager();
             
