45b967ccbbdf2aaa76b18b13820b30d7c0b69999
Guiks <guillaume.delapre@gmail.com>
2016-03-03 16:34:43 +0100

    Solve merge issue

--- src/GamingSchoolBundle/Controller/UserController.php
+++ src/GamingSchoolBundle/Controller/UserController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use GamingSchoolBundle\Form\SelectPack;
-use GamingSchoolBundle\Entity\Selling;
+use GamingSchoolBundle\Entity\Selling;  
 
 class UserController extends Controller
 {
@@ -18,39 +18,58 @@ class UserController extends Controller
      */
     public function coachesByGameAction($game_slug)
     {
-    	$userRepository = $this
-		  ->getDoctrine()
-		  ->getManager()
-		  ->getRepository('GamingSchoolBundle:User')
-		;
+     $userRepository = $this
+    ->getDoctrine()
+    ->getManager()
+    ->getRepository('GamingSchoolBundle:User')
+  ;
 
-		$gameRepository = $this
-		  ->getDoctrine()
-		  ->getManager()
-		  ->getRepository('GamingSchoolBundle:game')
-		;
+  $gameRepository = $this
+    ->getDoctrine()
+    ->getManager()
+    ->getRepository('GamingSchoolBundle:game')
+  ;
 
-		$game = $gameRepository->findOneBy(array('gameSlug' => $game_slug));
-		$listCoaches = $userRepository->findByCoaches($game->getId());
-		$data["game"] = $game->getGameName();
+  $game = $gameRepository->findOneBy(array('gameSlug' => $game_slug));
+  $listCoaches = $userRepository->findByCoaches($game->getId());
+  $data["game"] = $game->getGameName();
 
-		foreach ($listCoaches as $coach) {
-			$data["listCoaches"][] = array(
-				'id' => $coach->getId(),
-			 	'username' => $coach->getUsername(),
-			 	'firstname' => $coach->getUserFirstName(),
-			 	'lastname' => $coach ->getUserLastName(),
-			 	'email' => $coach->getEmail()
-			);
-		}
+  foreach ($listCoaches as $coach) {
+   $data["listCoaches"][] = array(
+    'id' => $coach->getId(),
+     'username' => $coach->getUsername(),
+     'firstname' => $coach->getUserFirstName(),
+     'lastname' => $coach ->getUserLastName(),
+     'email' => $coach->getEmail()
+   );
+  }
         return $this->render('GamingSchoolBundle:Default:listcoaches.html.twig', $data);
     }
+
     /**
-     * @Route("/profile/coach/{coach_id}")
      * @Route("/profile/user/{user_id}", name="userprofile")
      */
-    public function coachByIdAction($coach_id, Request $request)
     public function userProfileAction(Request $request, $user_id)
+    {
+     $userRepository = $this
+    ->getDoctrine()
+    ->getManager()
+    ->getRepository('GamingSchoolBundle:User')
+  ;
+
+  $infosUser = $userRepository->find($user_id);
+  $data["infos"] = $infosUser;
+  
+  if($infosUser->hasRole('ROLE_COACH'))
+   return $this->redirect('/profile/coach/'.$user_id.'');
+  else if($infosUser->hasRole('ROLE_USER'))
+   return $this->render('GamingSchoolBundle:Default:profile.html.twig', $data);
+ }
+    
+    /**
+     * @Route("/profile/coach/{coach_id}")
+     */
+    public function coachByIdAction($coach_id, Request $request)
     {
         $userRepository = $this
           ->getDoctrine()
@@ -66,32 +85,18 @@ class UserController extends Controller
                 'nb_hours' => $pack->getCoachingPackNbHours(),
                 'price' => $pack->getCoachingPackPrice(),
             );
-    	$userRepository = $this
-		  ->getDoctrine()
-		  ->getManager()
-		  ->getRepository('GamingSchoolBundle:User')
-		;
-
-		$infosUser = $userRepository->find($user_id);
-		$data["infos"] = $infosUser;
-		
-		if($infosUser->hasRole('ROLE_COACH'))
-			return $this->redirect('/profile/coach/'.$user_id.'');
-		else if($infosUser->hasRole('ROLE_USER'))
-			return $this->render('GamingSchoolBundle:Default:profile.html.twig', $data);
-	}
         }
         $form = $this->createForm(SelectPack::Class, $data["packs"])->createView();
         
-        $form->handleRequest($request);
+        //$form->handleRequest($request);
         
-        if($form->isSubmitted() && $form->isValid()){
+       /* if($form->isSubmitted() && $form->isValid()){
             $em = $this->get('doctrine')->getManager();
             
             $selling = new Selling();
             $em->persist($selling);
             $em->flush();
-        }
+        }*/
         /*$data['form']->handleRequest($_REQUEST);
         if ($data['form']->isSubmitted() && $data['form']->isValid()) {
         // ... perform some action, such as saving the task to the database
