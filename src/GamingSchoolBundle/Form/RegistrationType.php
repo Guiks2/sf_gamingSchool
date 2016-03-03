abea80a039697ea5c5c1bec6fbf6a572dd6f7438
eric <eric.pereirinha@hotmail.fr>
2016-03-03 10:52:48 +0100

    Correction de certains champs dans la page register

--- src/GamingSchoolBundle/Form/RegistrationType.php
+++ src/GamingSchoolBundle/Form/RegistrationType.php
@@ -5,15 +5,20 @@ namespace GamingSchoolBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+const textType = 'Symfony\Component\Form\Extension\Core\Type\TextType';
+const emailType = 'Symfony\Component\Form\Extension\Core\Type\EmailType';
+
 class RegistrationType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('userfirstname')
-			->add('userLastname')
-			->add('userAddress')
-			->add('userPhone')
+			->add('username', textType, array('label' => 'Nom de compte'))
+			->add('email', emailType, array('label' => 'eMail'))
+			->add('userfirstname', textType, array('label' => 'Prénom'))
+			->add('userLastname', textType, array('label' => 'Nom'))
+			->add('userAddress', textType, array('label' => 'Adresse'))
+			->add('userPhone', textType, array('label' => 'Téléphone'))
 		;
 	}
 	
