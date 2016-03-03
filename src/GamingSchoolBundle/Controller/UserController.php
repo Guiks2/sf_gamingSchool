5d58afffb7d8b797dc85b25afef5f3a2cd7558b1
Guiks <guillaume.delapre@gmail.com>
2016-03-03 16:06:08 +0100

    Mise en place du formulaire

--- src/GamingSchoolBundle/Controller/UserController.php
+++ src/GamingSchoolBundle/Controller/UserController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use GamingSchoolBundle\Form\SelectPack;
 
 class UserController extends Controller
 {
@@ -42,4 +43,37 @@ class UserController extends Controller
 		}
         return $this->render('GamingSchoolBundle:Default:listcoaches.html.twig', $data);
     }
+    
+    /**
+     * @Route("/profile/coach/{coach_id}")
+     */
+    public function coachByIdAction($coach_id)
+    {
+        $userRepository = $this
+          ->getDoctrine()
+          ->getManager()
+          ->getRepository('GamingSchoolBundle:User')
+        ;
+        $data['coach'] = $userRepository->find($coach_id);
+        
+        $packs = $data['coach']->getCoachingPack();
+        foreach ($packs as $pack) {
+            $data["packs"][] = array(
+                'id' => $pack->getId(),
+                'nb_hours' => $pack->getCoachingPackNbHours(),
+                'price' => $pack->getCoachingPackPrice(),
+            );
+        }
+        $data['form'] = $this->createForm(SelectPack::Class, $data["packs"])->createView();
+        
+        /*$data['form']->handleRequest($_REQUEST);
+        if ($data['form']->isSubmitted() && $data['form']->isValid()) {
+        // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('task_success');
+        }*/
+        
+        return $this->render('GamingSchoolBundle:Default:coach.html.twig', $data);
+    }
 }
+
